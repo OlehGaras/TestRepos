cb458f64817b9c513a04e637436f78f5cd6a4b91
OlehGaras <you@example.com>
2013-12-11 22:45:49 +0200

    first commit

---
+++ thread/thread/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace thread
+{
+    class Program
+    {
+        int time = 1000;
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(f);
+            t.Start(1000);
+            f(1000);
+        }
+        static void f(Object o)
+        {
+            lock (new object() )
+            {
+                Thread.Sleep((int)o);
+                Console.WriteLine((int)o);
+            }
+        }
+    }
+}
